b232c4ab393c383f8ea6f0bb93f3bb6d35b450da
Superboyfan <72908073+Superboyfan@users.noreply.github.com>
2020-11-07 18:30:12 +0530

    Update lydia.py

--- cinderella/modules/lydia.py
+++ cinderella/modules/lydia.py
@@ -87,7 +87,7 @@ def lydia(bot: Bot, update: Update):
             bot.send_message(OWNER_ID, f"lydia error: {e} occurred in {chat_id}!")
                     
 
-__mod_name__ = "PIKACHU"
+__mod_name__ = "RED"
 
 __help__ = """
 Commands

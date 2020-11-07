ad05db002ecd6ee55a6b56b38fae380175a4c6d0
Superboyfan <72908073+Superboyfan@users.noreply.github.com>
2020-11-07 17:15:49 +0530

    Update __main__.py

--- cinderella/__main__.py
+++ cinderella/__main__.py
@@ -78,7 +78,7 @@ GDPR = []
 
 START_IMG = os.environ.get('START_IMG', None)
 if START_IMG is None:
-    img = "https://telegra.ph/file/511ad504656e712b88235.jpg"
+    img = "https://telegra.ph/file/1948efded4fa0f9ced8c0.jpg"
 else:
   img = START_IMG    
     

72d902a3cd88abf8660f15e1f843d4e39ea5ceb5
Superboyfan <72908073+Superboyfan@users.noreply.github.com>
2020-11-08 15:38:01 +0530

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,12 +1,12 @@
 @echo off
-TITLE CinderellaProBot
+TITLE TrainerredBot
 rem This next line removes any fban csv files if they exist in root when bot restarts. 
 del *.csv
 py -3.7 --version
 IF "%ERRORLEVEL%" == "0" (
-    py -3.7 -m cinderella
+    py -3.7 -m pokemonred
 ) ELSE (
-    py -m cinderella
+    py -m pokemonred
 )
 
 pause

f402d559c2e08ea7c681c55f9995998b0d335445
Superboyfan <72908073+Superboyfan@users.noreply.github.com>
2020-11-08 20:53:11 +0530

    Update music.py

--- pokemon/modules/music.py
+++ pokemon/modules/music.py
@@ -61,7 +61,7 @@ def music(bot: Bot, update: Update, args):
 		if not json2_response['error']:
 			payload = json2_response['payload']
 
-			info = '*{0}* \nUploaded by CINDERELLA'.format(payload['fulltitle'])
+			info = '*{0}* \nUploaded by POKEMON'.format(payload['fulltitle'])
 
 			try:
 				

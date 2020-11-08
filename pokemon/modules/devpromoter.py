b9bf9b775ab28b78ed226e9fab576e9afe6022fd
Superboyfan <72908073+Superboyfan@users.noreply.github.com>
2020-11-08 21:00:35 +0530

    Update and rename botpromote.py to devpromoter.py

--- pokemon/modules/devpromoter.py
+++ pokemon/modules/devpromoter.py
@@ -13,7 +13,7 @@ from pokemon.modules.helper_funcs.chat_status import whitelist_plus, dev_plus
 from pokemon.modules.helper_funcs.extraction import extract_user
 from pokemon.modules.log_channel import gloggable
 
-ELEVATED_USERS_FILE = os.path.join(os.getcwd(), 'cinderella/elevated_users.json')
+ELEVATED_USERS_FILE = os.path.join(os.getcwd(), 'pokemon/elevated_users.json')
 
 
 def check_user_id(user_id: int, bot: Bot) -> Optional[str]:

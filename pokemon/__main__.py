b2fbe3512dcc982b423efce3571b80620d31d68f
Najah Muhabbik <72481862+frostid@users.noreply.github.com>
2020-11-13 03:34:46 +0700

    Update __main__.py

--- pokemon/__main__.py
+++ pokemon/__main__.py
@@ -182,7 +182,7 @@ def send_start(bot, update):
     first_name = update.effective_user.first_name 
     text = PM_START_TEXT
 
-    keyboard = [[InlineKeyboardButton(text="Menu",callback_data="help_back"),InlineKeyboardButton(text="Updates Channel",url="https://t.me/FrostClouds")]]
+    keyboard = [[InlineKeyboardButton(text="Menu/Help",callback_data="help_back"),InlineKeyboardButton(text="Updates Channel",url="https://t.me/FrostClouds")]]
     keyboard += [[InlineKeyboardButton(text="Support Group", url="https://t.me/FrostSupport"),InlineKeyboardButton(text="✅ Add Frost Manager to your group",url="t.me/{}?startgroup=true".format(bot.username))]]
 
     update.effective_message.reply_photo(img, PM_START_TEXT.format(escape_markdown(first_name), escape_markdown(bot.first_name), OWNER_NAME, OWNER_ID), 

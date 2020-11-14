437d1f664e59089d449563f0e2eda2c7c10b8dd7
Superboyfan <72908073+Superboyfan@users.noreply.github.com>
2020-11-14 11:37:40 +0530

    Update animequotesstring.py

--- pokemon/modules/animequotesstring.py
+++ pokemon/modules/animequotesstring.py
@@ -1,11 +1,9 @@
 ANIMEQUOTES=(
-"Typical of Goku to say something that crazy…
-  reply_markup=InlineKeyboardMarkup([[
-                    InlineKeyboardButton(
-                        text="Help",
-                        url="t.me/{}?start=ghelp_{}".format(
-                            context.bot.username, module))
-                ]]))",
+
+ "update.effective_message.reply_text("Contact me in PM to get the list of possible commands.",
+                                            reply_markup=InlineKeyboardMarkup(
+                                                [[InlineKeyboardButton(text="👑Help",url="t.me/{}?start=help".format(bot.username))],  
+                                                [InlineKeyboardButton(text="Ash Ketchum",url="https://t.me/AshKetchumRobot")]]))",
 "Excuse me but are you by any chance the oddball in your family?– Ash Ketchum",
 
 )

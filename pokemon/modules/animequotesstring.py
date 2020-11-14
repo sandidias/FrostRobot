d5bf94b07d55e270df1195bac9447f818bce8761
Superboyfan <72908073+Superboyfan@users.noreply.github.com>
2020-11-14 12:05:28 +0530

    Update animequotesstring.py

--- pokemon/modules/animequotesstring.py
+++ pokemon/modules/animequotesstring.py
@@ -1,8 +1,7 @@
 ANIMEQUOTES=(
-
-❝ "Believing in someone." That's a weird phrase, isn't it? After all, if you truly believe in a person, you wouldn't need to say, "I believe in you." It's like saying, "... ❞
-
-- Satoru Fujinuma,
-"Excuse me but are you by any chance the oddball in your family?– Ash Ketchum",
+  
+"❝Excuse me but are you by any chance the oddball in your family?❞
+  
+  – Ash Ketchum",
 
 )

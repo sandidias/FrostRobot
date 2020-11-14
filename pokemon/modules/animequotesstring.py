734c1263c7bfe68ea3e8dfbc6cb2d6d66edfac51
Superboyfan <72908073+Superboyfan@users.noreply.github.com>
2020-11-14 10:50:00 +0530

    Update animequotesstring.py

--- pokemon/modules/animequotesstring.py
+++ pokemon/modules/animequotesstring.py
@@ -1,6 +1,5 @@
 ANIMEQUOTES=(
 "Typical of Goku to say something that crazy…"
 "Excuse me but are you by any chance the oddball in your family?– Ash Ketchum",
-"L Lawliet-f you use your head, you won’t get fat even if you eat sweets.”,
 
 )

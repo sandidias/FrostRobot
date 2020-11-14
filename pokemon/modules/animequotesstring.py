4d62054588eaf75d75f41614a10c9221031dba30
Superboyfan <72908073+Superboyfan@users.noreply.github.com>
2020-11-14 12:51:55 +0530

    Update animequotesstring.py

--- pokemon/modules/animequotesstring.py
+++ pokemon/modules/animequotesstring.py
@@ -1,7 +1,5 @@
 ANIMEQUOTES=(
 "❝Typical of Goku to say something that crazy…❞",
-"❝Excuse me but are you by any chance the oddball in your family?❞
-  
-  – Ash Ketchum",
+"❝Excuse me but are you by any chance the oddball in your family?❞– Ash Ketchum",
 
 )

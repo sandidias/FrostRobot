d1bc6d38c36706b03edec33f2b7b4b689b9fb0af
Superboyfan <72908073+Superboyfan@users.noreply.github.com>
2020-11-14 13:44:15 +0530

    Update animequotes.py

--- pokemon/modules/animequotes.py
+++ pokemon/modules/animequotes.py
@@ -1,6 +1,5 @@
 #Made By @Madepranav On Telegram & Github Id Superboyfam
 import html
-import animequotes
 import random
 import pokemon.modules.animequotesstring as animequotesstring
 from pokemon import dispatcher

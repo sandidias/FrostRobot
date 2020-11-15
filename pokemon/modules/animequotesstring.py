9943d19f0ce48d895d4eec69b85ac10562ad29e2
Superboyfan <72908073+Superboyfan@users.noreply.github.com>
2020-11-15 07:58:15 +0530

    Update animequotesstring.py

--- pokemon/modules/animequotesstring.py
+++ pokemon/modules/animequotesstring.py
@@ -1,4 +1,4 @@
-ANIMEQUOTES_TEMPLATES=(
+ANIMEQUOTES=(
 "Typical of Goku to say something that crazy…",
 "Excuse me but are you by any chance the oddball in your family?– Ash Ketchum",
 "The world isn't perfect. But it's there for us, doing the best it can....that's what makes it so damn beautiful.~ Roy Mustang (Full Metal Alchemist)",
